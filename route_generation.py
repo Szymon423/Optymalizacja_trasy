630f2ec8d8291eb84f79bef4f721b81985971baf
Szymon <wachim17@gmail.com>
2022-04-20 16:13:44 +0200

    test1

--- route_generation.py
+++ route_generation.py
@@ -4,5 +4,5 @@ import numpy as np
 
 def generate_route():
     X = np.array([i for i in range(2000)])
-    Y = np.sin(X*2*np.pi/2000)*200 + np.sin(X*2*np.pi/200)*2
+    Y = np.sin(X*2*np.pi/2000)*200 + np.sin(X*2*np.pi/200)*2 + (np.random.rand(2000)*10 - 1) * 2
     return X, Y
